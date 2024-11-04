64b92c89dca052909f011a9cb36cc81d3b146824
head <headshot_1998@mail.ru>
2024-11-04 14:10:51 +0400

    Добавьте файлы проекта.

---
+++ PrototypeOtus/Program.cs
@@ -0,0 +1,28 @@
+﻿using PrototypeOtus.Prototype;
+
+namespace PrototypeOtus
+{
+    internal class Program
+    {
+        public static void Main(string[] args) 
+        {
+            Console.WriteLine("Создайте продукт: ");
+            Console.WriteLine("Введите название: ");
+            string name = Console.ReadLine();
+
+            Console.Write("Введите цену: ");
+            Double price = Convert.ToDouble(Console.ReadLine());
+
+            ConcretePrototype originalProduct  = new ConcretePrototype(name, price);
+            Console.WriteLine($"оригинальный продукт создан: {originalProduct}");
+
+            Console.WriteLine("создать клон продукта? (да/нет)");
+            if (Console.ReadLine()?.ToLower() == "да")
+            {
+                Product clonnedProduct = originalProduct.Clone();
+                Console.WriteLine($"клон продукта создан {clonnedProduct}");
+            }
+        }
+
+    }
+}
---
+++ PrototypeOtus/Prototype/Prototype.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PrototypeOtus.Prototype
+{
+    public abstract class Product
+    {
+        public string Name { get; set; }
+
+        public double Price {  get; set; }
+        public abstract Product Clone();
+    }
+
+    // релазиация клонирование через prototype
+    class ConcretePrototype : Product
+    {
+        public ConcretePrototype(string name, double price)
+        {
+            Name = name;
+            Price = price;
+        }
+
+        // Реализация метода клонирования
+        public override Product Clone() 
+        {
+            return (Product)MemberwiseClone();
+        }
+
+        public override string ToString() 
+        {
+            return $"Продукт {Name}, Цена: {Price}";
+        }
+
+    }
+}
